2b94d8a0548bb8165fcb7114e295cd794997fdf7
Matis ZOUARI <matis.zouari@insa-lyon.fr>
2025-04-29 11:43:54 +0400

    Removed testing code

--- src/app/notes/[id]/page.tsx
+++ src/app/notes/[id]/page.tsx
@@ -1,14 +1,5 @@
-import { Note } from '../../../types'; // Import the Note type
 import { notFound } from 'next/navigation'; // for handling 404 errors
-import Link from 'next/link';
-
 import { prisma } from '@/lib/prisma';
-
-const notes: Note[] = [
-  { id: 1, title: 'Note 1', content: 'This is the full content of note 1...', preview: 'This is the preview of note 1...' },
-  { id: 2, title: 'Note 2', content: 'This is the full content of note 2...', preview: 'This is the preview of note 2...' },
-  { id: 3, title: 'Note 3', content: 'This is the full content of note 3...', preview: 'This is the preview of note 3...' }
-];
 
 // Your dynamic page component
 export default async function NotePage({ params }: { params: { id: string } }) {
